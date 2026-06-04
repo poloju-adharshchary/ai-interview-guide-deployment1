da4ec2360ab9bec4fb1df2cc289966c46267da0f
Poloju Adharsh <polojuadharshchary@gmail.com>
2026-06-04 14:02:09 +0530

    Fix Railway DB connection

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,9 @@ DB_NAME = os.getenv("DB_NAME")
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD")
+    "password": os.getenv("DB_PASSWORD"),
+    "port": os.getenv("DB_PORT")
+
 }
 
 def create_database():
@@ -18,6 +20,7 @@ def create_database():
 def get_db():
     return mysql.connector.connect(
         host=DB_CONFIG["host"],
+        port=int(DB_CONFIG["port"]),
         user=DB_CONFIG["user"],
         password=DB_CONFIG["password"],
         database=DB_NAME
